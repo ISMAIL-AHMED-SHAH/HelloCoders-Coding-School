20d798140b94e22d8f43b19197d9c4a64aebea47
Ismail Ahmed <aismail@kictl.com>
2024-10-12 04:22:54 +0500

    contact page added

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+// src/app/contact/page.tsx
+
+'use client';
+
+function ContactPage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-2xl font-bold">Contact Us</h1>
+            <p>You can reach us at ismailahmedshahpk@gmail.com or call us at +923322241405.</p>
+        </div>
+    );
+}
+
+export default ContactPage;
